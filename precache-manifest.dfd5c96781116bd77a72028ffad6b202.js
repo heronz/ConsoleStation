53433fd608580c80c4006d886c5dc6d77b702a92
Heron Vieira <heron.vieira@outlook.com.br>
2020-04-13 20:26:48 -0300

    Updates

---
+++ precache-manifest.dfd5c96781116bd77a72028ffad6b202.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3aaae56eafa4d23a9fbe1086211033e3",
+    "url": "/ConsoleStation/index.html"
+  },
+  {
+    "revision": "dd8b4d26231bee3ff86a",
+    "url": "/ConsoleStation/static/css/2.e5b99fdb.chunk.css"
+  },
+  {
+    "revision": "84a4554a79fdb90b40c8",
+    "url": "/ConsoleStation/static/css/main.e0103c51.chunk.css"
+  },
+  {
+    "revision": "dd8b4d26231bee3ff86a",
+    "url": "/ConsoleStation/static/js/2.b39e8b28.chunk.js"
+  },
+  {
+    "revision": "872d4ad223c39fec1068d862c0b329ff",
+    "url": "/ConsoleStation/static/js/2.b39e8b28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84a4554a79fdb90b40c8",
+    "url": "/ConsoleStation/static/js/main.1312dc04.chunk.js"
+  },
+  {
+    "revision": "ae36a0075ef2ac4e4cbd",
+    "url": "/ConsoleStation/static/js/runtime-main.4ebae1d0.js"
+  }
+]);
